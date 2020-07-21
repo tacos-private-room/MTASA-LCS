eecd984a866e44b863dd6b4e90389e70eff4cd2c
Nurupo <episodes27@gmail.com>
2020-07-21 19:01:14 +1000

    add gui lib

--- mods/deathmatch/resources/[LCS]/lcs_gMenu/menu.lua
+++ mods/deathmatch/resources/[LCS]/lcs_gMenu/menu.lua
@@ -1,7 +1,24 @@
-function mainMenu()
+DGS = exports.dgs
 
+label = DGS:dgsCreateLabel(0,0,1,1,"#FFFFFFSTART GAME\n#78aafaOPTIONS\nCREDIT\nEXIT GAME",true)
+DGS:dgsSetProperty(label,"alignment",{"center","center"})
+DGS:dgsSetProperty(label,"textSize",{1.7,1.7})
+DGS:dgsSetProperty(label,"shadow",{1,1,tocolor(0,0,0,255),5})
+DGS:dgsSetProperty(label,"font","bankgothic")
+DGS:dgsSetProperty(label,"textColor",tocolor(120,170,250,255))
+DGS:dgsSetProperty(label,"colorcoded",true)
+
+label2 = DGS:dgsCreateLabel(0,0,1,1,"MTA:LCS - DEV BUILD (By nurupo)",true)
+DGS:dgsSetProperty(label2,"alignment",{"left","bottom"})
+DGS:dgsSetProperty(label2,"font","sans")
+
+
+
+
+
+function mainMenu()
     local x,y = guiGetScreenSize ()
-    dxDrawImage ( 0, 0, x, y, "loadsc0.png")
+    dxDrawImage ( 0, 0, x, y, "frontend_background.png")
 end
 
 
